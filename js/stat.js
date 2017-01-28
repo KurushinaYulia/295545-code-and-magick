2c8e7a2c3ea269e8a77146292445ca4adfa43f80
Kurushina Yulia <kurushina.yulia@gmail.com>
2017-01-28 23:36:07 +0800

    code in super functions

--- js/stat.js
+++ js/stat.js
@@ -1,57 +1,59 @@
-'use strict'
+'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+function rectangle(ctx, x, y, widthR, heightR, r, g, b, a) {
+  ctx.fillStyle = 'rgba('+r+','+ g+','+ b+','+ a+')';
+  ctx.strokeRect(x, y, widthR, heightR);
+  ctx.fillRect(x, y, widthR, heightR);
+}
 
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
-
+function message(ctx, yourMessage, x, y) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
+  ctx.fillText(yourMessage, x, y);
+}
 
-  ctx.fillText('Ура! Вы победили!', 220, 40);
-  ctx.fillText('Список результатов:', 150, 60);
-
+function maxTime(times) {
   var max = -1;
-
-  for (var i = 0; i < times.length; i++) {
+  for ( var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
     }
   }
+  return max;
+}
 
-  var histoHeight = 150;
-  var columnWidth = 40;
-  var histoX = 140;
-  var step = histoHeight / max;
-  var columnIndent = 50;
+function randomBlue() {
+  return 'rgba(0, 0, ' + (Math.random() * 250).toFixed(0) + ',1)';
+}
+
+
+function columnDraw(ctx, name, time, height, position, settings) {
+  message(ctx,time.toFixed(0),settings.histoX + settings.columnWidth * position + settings.columnIndent * position, 1.6 * settings.histoHeight - height);
+
+  ctx.fillStyle = ( name === "Вы" ) ? 'rgba(255, 0, 0, 1)' : randomBlue();
+  ctx.fillRect(settings.histoX + settings.columnWidth * position + settings.columnIndent * position, 1.6 * settings.histoHeight - height, settings.columnWidth, height);
+
+
+  message(ctx,name,settings.histoX + settings.columnWidth * position + settings.columnIndent * position, 260);
+}
+
+function diagramDraw(ctx, times, names, settings) {
+  var step = settings.histoHeight / maxTime(times);
 
   for (var i = 0; i < times.length; i++) {
-    var name = names[i];
-    var time = times[i];
-    var height = step * time;
-
-
-    ctx.fillText(time.toFixed(0), histoX + columnWidth*i + columnIndent * i, 1.6 * histoHeight - height);
-
-    if (names[i] == 'Вы'){
-      ctx.fillStyle = 'rgba (255, 0, 0, 1)';
-    } else ctx.fillStyle = ['rgb (0, 0,', ((Math.random() * 5) * 50).toFixed(0),')'];
-
-
-    ctx.fillRect(histoX + columnWidth*i + columnIndent * i, 1.6 * histoHeight - height, columnWidth, height);
-    ctx.fillStyle = '#000';
-    ctx.fillText(name, histoX + columnWidth*i + columnIndent * i, 260);
+    var height = step * times[i];
+    columnDraw(ctx, names[i], times[i], height, i, settings);
   }
+}
+
+window.renderStatistics = function(ctx, names, times) {
+
+  rectangle(ctx,110, 20, 420, 270, 0, 0, 0, 0.7);
+  rectangle(ctx,100, 10, 420, 270, 256, 256, 256, 1.0);
+
+  message(ctx, 'Ура! Вы победили!', 220, 40);
+  message(ctx, 'Список результатов:', 150, 60);
+  diagramDraw(ctx, times, names, { histoHeight : 150, columnWidth : 40, histoX : 140, columnIndent : 50 });
 };
-
-
-
-
-
-
-
 
